89998f03a8f6a5e7bddf1c5f91352d7f718f676a
fadimata tillman <fadi.tillman@gmail.com>
2020-04-19 23:51:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,6 +61,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
+  Movie.create(title)
   # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
