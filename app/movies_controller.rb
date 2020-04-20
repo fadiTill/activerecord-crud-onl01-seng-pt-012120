3722e27a5b483cb43d3dfd8098ae70694eb0fbef
fadimata tillman <fadi.tillman@gmail.com>
2020-04-20 00:00:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -72,7 +72,7 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   __
   __
-  Movie.where(title:"Awesome Flick").update()
+  Movie.where(title:"Awesome Flick").update(title:"Even Awesomer Flick")
 end
 
 def can_update_using_update_method
