9b558cc4631e391bed5c11763f392ea01c5c3fda
fadimata tillman <fadi.tillman@gmail.com>
2020-04-20 00:06:25 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -78,7 +78,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
+
   Movie.create(title:"wat?").update(title: "Wat, huh?")
 end
 
